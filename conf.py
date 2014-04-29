416e0e4bad3e4772aa763e59553afd18a683023f
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-04-29 11:28:16 -0400

    Remove merge conflict and improve Read the Docs theme compatibility

--- conf.py
+++ conf.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys, os
 
-try:
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-except ImportError:
-    html_theme = 'default'
-
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -63,10 +55,17 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-<<<<<<< HEAD
-=======
-html_theme = 'sphinx_rtd_theme'
->>>>>>> 526a2ec1efdc9ff5cc39a5e56144adeb797d45e6
+
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    try:
+        import sphinx_rtd_theme
+        html_theme = 'sphinx_rtd_theme'
+        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    except ImportError:
+        html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
